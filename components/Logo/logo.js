44df0aa7caa5b96a1ff1a400e594b07c89c934b0
PSkinnerTech <patrick@clgcorporation.com>
2023-03-10 17:51:44 -0500

    remove sample files

---
+++ components/Logo/logo.js
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <div className="text-3xl text-center py-8 px-4 font-heading">
+      <img src="/logo.png" />
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
+import { Logo } from '../components/Logo';
+import Link from 'next/link';
+import Image from 'next/image';
+import HeroImage from '../public/hero.webp';
+
 export default function Home() {
-  return <div />;
+  return (
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} at="Hero" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/50 rounded-md backdrop-blur-sm">
+        <Logo />
+        <p className="py-4 px-4">
+          The AI-powered platform designed to train you to close deals.
+        </p>
+        <Link href="/post/new" className="btn ">
+          Begin
+        </Link>
+      </div>
+    </div>
+  );
 }
